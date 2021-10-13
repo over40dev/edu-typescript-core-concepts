eeeb6dc80f27dd6a5087aecf1408a47f9a9adae7
Cogentx <code@cogentx.com>
2021-10-13 16:25:03 -0230

    Working with Types in TypeScript

--- src/types.ts
+++ src/types.ts
@@ -17,19 +17,20 @@ let foo2 : undefined = undefined;
 let foo3 : null = null;
 
 // can't assign number to string in TS lastName = 5; can't add number to string
-// array in TS pets.push(5); no data types including 'interfaces' do not make it
-// into plain JS once compiled interfaces are like 'code contracts' that specify
-// the property and 'type' and can be labelled optional
-interface Customer {
+// array in TS pets.push(5); no data types including 'interfaces' do not make
+// it into plain JS once compiled interfaces are like 'code contracts' that
+// specify the property and 'type' and can be labelled optional
+interface Friend {
   firstName : string;
   lastName : string;
+  age : number;
 }
 
-// this causes TS error because 'customer' is defined as type 'Customer' which
+// this causes TS error because 'friend' is defined as type 'Friend' which
 // knows it should have two string properties named 'firstName' and 'lastName'
-// but only one is present. let customer: Customer = {   firstName: 'Ted' } now
+// but only one is present. let friend: Friend = {   firstName: 'Ted' } now
 // there are no TS errors because all properties are provide
-let customer : Customer = {
+let friend : Friend = {
   firstName: 'Jane',
   lastName: 'Doe',
   age: 27
