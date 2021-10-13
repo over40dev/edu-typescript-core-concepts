c99efb0490111d6c12ff3e7da86e0e535b42226f
Cogentx <code@cogentx.com>
2021-10-13 17:01:19 -0230

    Using interfaces in TypeScrip (class option)

---
+++ src/interfaces.ts
@@ -0,0 +1,18 @@
+// This works... but it actually compiles to a larger bundle size because when it is converted to regular JavaScript the Class has to be structured.
+// We have another alternative in TypeScript (and in other Class based languages like C#, Java, etc.) and that is called Interfaces.
+// Interfaces are essentially code contracts
+// Interfaces allows us to enforce the shape of data expected (including optional properties).
+class Settings {
+  title = '';
+  color = '';
+}
+
+function initialize(settings: Settings) {
+console.log(settings);
+}
+
+initialize({
+  title: 'Blue',
+  color: 'blue',
+});
+
